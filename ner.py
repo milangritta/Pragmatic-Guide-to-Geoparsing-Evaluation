35b6ecc1827edf4ac5d1176e19f61dbf08e10ccd
Milan Gritta <you@example.com>
2018-08-13 08:51:54 +0100

    Added toponym lists for noun phrases, whole and partial. Working on the train/dev/test splits, fixed the random seeds, still quite a bit to do.

--- ner.py
+++ ner.py
@@ -5,11 +5,13 @@ import spacy
 import random
 from objects_and_functions import text_to_ann, ANNOT_SOURCE_DIR, transform_tags
 
-random.seed(111111)
+random.seed(1111)
 
 annotations = text_to_ann()
-toponyms = codecs.open("data/toponyms.txt", encoding="utf-8")
-toponyms = [t.strip() for t in toponyms]
+m_toponyms = codecs.open("data/toponyms.txt", encoding="utf-8")
+m_toponyms = [t.strip() for t in m_toponyms]
+n_toponyms = codecs.open("data/noun_toponyms.txt", encoding="utf-8")
+n_toponyms = [t.strip() for t in n_toponyms]
 nlp = spacy.load('en_core_web_lg')
 all_files = []
 # label_map = {u"Literal": u"Lit", u"Homonym": u"NonLit", u"Coercion": u"Lit", u"Mixed": u"Mixed", u"Embedded": u"Mixed",
@@ -25,7 +27,9 @@ for file_name in annotations:
     all_sentences = []
     annots = dict([(int(annotations[file_name][x].start), annotations[file_name][x]) for x in annotations[file_name]])
     for sentence in nlp(text).sents:
-        output = []
+        sentence_output = []
+        np_heads = []
+        replacement = []
         is_augmented = False
         is_annotated = False
         label, ann, index = u"0", 0, 0
@@ -46,18 +50,25 @@ for file_name in annotations:
                     index = word.idx + offset
                 else:
                     is_augmented = True
-                    top = nlp(toponyms[random.randint(0, len(toponyms) - 1)])
+                    np_heads.append(word)
+                    top = nlp(m_toponyms[random.randint(0, len(m_toponyms) - 1)])
                     for t in top:
-                        head = word.text if t.head == t else t.head.text
-                        output.append(t.text + u" [POS]" + t.pos_ + u" [Shape]" + t.shape_ + u" [Head]" + head + u" Entity\n")
-            output.append(word.text + u" [POS]" + word.pos_ + u" [Shape]" + word.shape_ + u" [Head]" + word.head.text + u" " + label_map.get(label, u"0") + "\n")
-            # RAW FILE? DECODE SETUP? NP AUGMENTATION?
+                        # head = word.text if t.head == t else t.head.text
+                        # output.append((t.text + u" [Shape]" + t.shape_ + u" [Head]" + head + u" Entity\n", True))
+                        sentence_output.append((t.text + u" [Shape]" + t.shape_ + u" Entity\n", True, word.i - t.i))
+                    top = nlp(n_toponyms[random.randint(0, len(n_toponyms) - 1)])
+                    for t in top:
+                        replacement.append((t.text + u" [Shape]" + t.shape_ + u" Entity\n", True, word.i - t.i))
+            # output.append((word.text + u" [Shape]" + word.shape_ + u" [Head]" + word.head.text + u" " + label_map.get(label, u"0") + "\n", False))
+            sentence_output.append((word.text + u" [Shape]" + word.shape_ + u" " + label_map.get(label, u"0") + "\n", False, word.i))
+            # RAW FILES? DECODE SETUP? F-Score per class! NP AUGMENTATION? How does BMES actually work?
             if label != u"0":
                 index += len(word) + 1
             if ann != 0 and index - 1 >= int(annots[ann].end):
                 label = u"0"
-        output.append(u"\n")
-        all_sentences.append((output, is_augmented and not is_annotated))
+        sentence_output.append((u"\n", False, -1))
+        sentence_output = [a for (a, b, c) in sentence_output if not b] if is_augmented and is_annotated else [a for (a, b, c) in sentence_output]
+        all_sentences.append((sentence_output, is_augmented and not is_annotated))
     all_files.append(all_sentences)
 
 train = codecs.open("data/train.txt", mode="w", encoding="utf-8")
@@ -68,9 +79,9 @@ for all_sentences in all_files:
     r = random.random()
     for sentence, is_augmented in all_sentences:
         for word in sentence:
-            if r < 0.6 or is_augmented:
+            if r < 0.8 or is_augmented:
                 train.write(word)
-            elif r > 0.8:
+            elif r > 0.9:
                 dev.write(word)
             else:
                 test.write(word)
